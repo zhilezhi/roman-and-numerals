c0372161282eb8d7379f94bd61a0a22d37deacfe
zhilezhi <zhizhiafei@outlook.com>
2022-03-01 17:46:57 +0800

    :tada: 增加数字转换成罗马数字

--- index.js
+++ index.js
@@ -28,7 +28,41 @@ function transNumber(s){
 function checkRoman(s){
     return /^(-| +|^)M{0,9}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})( +|$)/.test(s);
 }
+
+/**
+ * 数字转换成罗马数字
+ * @param n
+ */
+function transRoman(n){
+    if(n > 3999 || n < 0){
+        throw '超出计算范围'
+    }
+    const roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let str = '';
+    for (const i of Object.keys(roman)) {
+        const q = Math.floor(n / roman[i]);
+        n -= q * roman[i];
+        str += i.repeat(q);
+    }
+    return str;
+}
 export {
     transNumber,
-    checkRoman
+    checkRoman,
+    transRoman
 }
+
